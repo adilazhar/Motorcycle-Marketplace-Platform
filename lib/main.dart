77dbd8d62e864a9ae6745256f4b55dd61bac0d29
Adil Azhar <adil.azhar838@gmail.com>
2024-12-27 11:53:36 +0500

    Setup firebase for local development

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bike_listing/firebase_options.dart';
 import 'package:bike_listing/my_app.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -11,9 +10,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-  await FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
-  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+
+  // TODO: Set This IP Before running
+  String host = '192.168.1.137';
+  await FirebaseStorage.instance.useStorageEmulator(host, 9199);
+  FirebaseFirestore.instance.useFirestoreEmulator(host, 8080);
 
   runApp(ProviderScope(child: MyApp()));
 }
