9c900dc51c1c3917feb6277f998f8b082b5f8497
Adil Azhar <adil.azhar838@gmail.com>
2025-01-22 13:23:20 +0500

    Changed the app font

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:bike_listing/src/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends ConsumerWidget {
   const MyApp({
@@ -12,6 +13,9 @@ class MyApp extends ConsumerWidget {
     final appRouter = ref.watch(appRouterProvider);
     return MaterialApp.router(
       routerConfig: appRouter,
+      theme: ThemeData(
+        textTheme: GoogleFonts.ralewayTextTheme(Theme.of(context).textTheme),
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Bike Listing',
     );
